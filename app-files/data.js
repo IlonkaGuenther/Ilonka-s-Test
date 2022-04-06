fc5898ed315d3c89e099488cf5ba0cde629d13bd
IlonkaGuenther <lacig@nus.edu.sg>
2022-04-06 11:36:37 +0800

    Test 2
    Test 2

---
+++ app-files/data.js
@@ -0,0 +1,152 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-along-engineering-drive-1",
+      "name": "Along Engineering Drive 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.2965942931172023,
+        "pitch": 0.007213760398599334,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.25769988424548274,
+          "pitch": -0.4019699085607904,
+          "rotation": 12.566370614359176,
+          "target": "1-engineering-auditorium-atrium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.4226903365757,
+          "pitch": -0.40593080343905363,
+          "title": "Engineering Drive<br>",
+          "text": "Just so you know<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-engineering-auditorium-atrium",
+      "name": "Engineering Auditorium Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.856253435360215,
+        "pitch": 0.27231945504710886,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8586374842157696,
+          "pitch": 0.2723187182048399,
+          "rotation": 0,
+          "target": "1-engineering-auditorium-atrium"
+        },
+        {
+          "yaw": 2.856253435360215,
+          "pitch": 0.27231945504710886,
+          "rotation": 0,
+          "target": "2-engineering-auditorium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1793954144548433,
+          "pitch": -0.10107945719301092,
+          "title": "Auditorium inside<br>",
+          "text": "On a quiet day ones upon a time"
+        }
+      ]
+    },
+    {
+      "id": "2-engineering-auditorium",
+      "name": "Engineering Auditorium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.3993651400323923,
+        "pitch": -0.09017295056894703,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3993651400323923,
+          "pitch": -0.09017295056894703,
+          "rotation": 0,
+          "target": "0-along-engineering-drive-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.084451126338884,
+          "pitch": -0.3429994476039653,
+          "title": "Engineering Auditorium<br>",
+          "text": "On a drairy day<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Ilonka Test marzipano",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
